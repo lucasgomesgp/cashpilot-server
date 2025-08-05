e34e2676098a362baf794d2d4423b3a2d13f3d95
Lucas Gomes <lucasgomesbt@gmail.com>
2025-08-05 18:28:12 -0300

    feat: prisma schema, model user and docker config

---
+++ prisma/migrations/20250805212534_add_model_user/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "imageUrl" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
